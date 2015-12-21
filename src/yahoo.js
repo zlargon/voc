00c676e2470cfb89e1c0ee4bd72c5c7e32e3e4d0
zlargon <zlargon@icloud.com>
2015-12-22 04:53:06 +0800

    [service] implement yahoo.js to get audio URL

---
+++ src/yahoo.js
@@ -0,0 +1,21 @@
+var Promise = require('promise');
+var fetch   = require('node-fetch');
+var cheerio = require('cheerio');
+
+module.exports = function (word) {
+  var HOST = 'http://tw.dictionary.search.yahoo.com';
+  if (typeof word !== 'string') {
+    return Promise.reject(new TypeError());
+  }
+
+  word = word.toLowerCase();
+  return fetch(HOST + '/search?p=' + word + '&fr2=dict')
+    .then(function(res) { return res.text(); })
+    .then(function(html) {
+      var $ = cheerio.load(html);
+      return $('#iconStyle.tri').text();
+    })
+    .then(function(data) {
+      return JSON.parse(data).sound_url_1[0].mp3;
+    });
+}
---
+++ test/yahoo.js
@@ -0,0 +1,45 @@
+var chai           = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var yahoo          = require('../src/yahoo');
+var expect         = chai.expect;
+
+// setup promise
+chai.use(chaiAsPromised);
+
+describe('Yahoo Dictionary', function() {
+
+  it('Hello', function () {
+    var word = 'Hello';
+    return expect(yahoo(word)).to.eventually.equal(
+      'https://s.yimg.com/tn/dict/dreye/live/f/hello.mp3'
+    );
+  });
+
+  it('test', function () {
+    var word = 'test';
+    return expect(yahoo(word)).to.eventually.equal(
+      'https://s.yimg.com/tn/dict/dreye/live/f/test.mp3'
+    );
+  });
+
+  it('askdjalksjdl', function () {
+    var word = 'askdjalksjdl';
+    return expect(yahoo(word)).to.eventually.be.rejected;
+  });
+
+  it('(null)', function () {
+    var word = null;
+    return expect(yahoo(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+  it('123 (number)', function () {
+    var word = 123;
+    return expect(yahoo(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+  it('123 (string)', function () {
+    var word = '123';
+    return expect(yahoo(word)).to.eventually.be.rejected;
+  });
+
+});
