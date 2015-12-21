9d67ee9265cad0c15cd4f10408d0965d8eec09e8
zlargon <zlargon@icloud.com>
2015-12-22 04:03:28 +0800

    [service] implement voicetube.js to get audio URL

---
+++ src/voicetube.js
@@ -0,0 +1,26 @@
+var Promise = require('promise');
+var fetch = require('node-fetch');
+
+module.exports = function (word) {
+  const HOST = 'https://tw.voicetube.com';
+  if (typeof word !== 'string') {
+    return Promise.reject(new TypeError());
+  }
+
+  word = word.toLowerCase();
+  return fetch(HOST + '/videos/ajax_get_search/word?q=' + word)
+    .then(function (res) {
+      return res.text();
+    })
+    .then(function (text) {
+      return text.split('\n');
+    })
+    .then(function(list) {
+      for (var i = 0; i < list.length; i++) {
+        if (list[i] === word) {
+          return HOST + '/player/' + word + '.mp3';
+        }
+      }
+      return Promise.reject(new Error(word + 'is not found'));
+    });
+}
---
+++ test/voicetube.js
@@ -0,0 +1,45 @@
+var chai           = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var voicetube      = require('../src/voicetube');
+var expect         = chai.expect;
+
+// setup promise
+chai.use(chaiAsPromised);
+
+describe('VoiceTube', function() {
+
+  it('Hello', function () {
+    var word = 'Hello';
+    return expect(voicetube(word)).to.eventually.equal(
+      'https://tw.voicetube.com/player/hello.mp3'
+    );
+  });
+
+  it('test', function () {
+    var word = 'test';
+    return expect(voicetube(word)).to.eventually.equal(
+      'https://tw.voicetube.com/player/test.mp3'
+    );
+  });
+
+  it('askdjalksjdl', function () {
+    var word = 'askdjalksjdl';
+    return expect(voicetube(word)).to.eventually.be.rejected;
+  });
+
+  it('(null)', function () {
+    var word = null;
+    return expect(voicetube(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+  it('123 (number)', function () {
+    var word = 123;
+    return expect(voicetube(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+  it('123 (string)', function () {
+    var word = '123';
+    return expect(voicetube(word)).to.eventually.be.rejected;
+  });
+
+});
