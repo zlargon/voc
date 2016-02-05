391fd7046a2326c9cf47f7e8954d3e2243c6c199
zlargon <zlargon@icloud.com>
2016-02-05 11:13:12 +0800

    [service] implement collins.js to get audio URL

---
+++ src/collins.js
@@ -0,0 +1,83 @@
+var util      = require('util');
+var coroutine = require('co');
+var fetch     = require('node-fetch');
+var cheerio   = require('cheerio');
+
+module.exports = function collins (word) {
+  return coroutine(function * () {
+    if (typeof word !== 'string' || word.length === 0) {
+      throw new TypeError('word should be a string');
+    }
+
+    // replace '_' to ' ', and convert to lower case
+    word = word.replace(/_/g, ' ').toLowerCase();
+
+    var HOST = 'http://www.collinsdictionary.com';
+    var url = HOST + '/search/autocomplete/AMERICAN_DICTIONARY?term=' + word;
+    var res = yield fetch(url, {
+      timeout: 10 * 1000
+    });
+    if (res.status !== 200) {
+      var msg = util.format('request to %s failed, status code = %d (%s)', url, res.status, res.statusText);
+      throw new Error(msg);
+    }
+
+    // find the word from list
+    var isFound = false;
+    var list = JSON.parse(yield res.text());
+    for (var i = 0; i < list.length; i++) {
+      if (list[i] === word) {
+        isFound = true;
+        break;
+      }
+    }
+
+    // word is not found
+    if (isFound === false) {
+      var err = new Error(word + ' is not found from collins');
+      err.code = 'ENOENT';
+      throw err;
+    }
+
+    // get the word page
+    var url = HOST + '/dictionary/american/' + word.replace(/ /g, '-');
+    var res = yield fetch(url, {
+      timeout: 10 * 1000
+    });
+    if (res.status !== 200) {
+      var msg = util.format('request to %s failed, status code = %d (%s)', url, res.status, res.statusText);
+      throw new Error(msg);
+    }
+
+    // parse HTML
+    var html = yield res.text();
+    var $ = cheerio.load(html);
+
+    var map = {};
+    var list = [];
+    $('a.hwd_sound').each(function (index, ele) {
+      var audio = HOST + $(ele).attr('data-src-mp3');
+      if (map.hasOwnProperty(audio) === false) {
+        map[audio] = true;
+        list.push(audio);
+      }
+    });
+
+    if (list.length === 0) {
+      var err = new Error(word + ' has no audio from collins');
+      err.code = 'ENOENT';
+      throw err;
+    }
+
+    // show all the audio url
+    if (list.length > 1) {
+      list.forEach(function (audio, i) {
+        var num = i + 1;
+        console.log(num + '. ' + audio);
+      });
+    }
+
+    // return the first audio from list
+    return list[0];
+  });
+}
---
+++ test/collins.js
@@ -0,0 +1,70 @@
+var chai           = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var collins        = require('../src/collins');
+var expect         = chai.expect;
+
+// setup promise
+chai.use(chaiAsPromised);
+
+describe('collins', function() {
+
+  it('Hello', function () {
+    this.timeout(10000);
+    var word = 'Hello';
+    return expect(collins(word)).to.eventually.equal(
+      'http://www.collinsdictionary.com/sounds/e/en_/en_us/en_us_hello.mp3'
+    );
+  });
+
+  it('test', function () {
+    this.timeout(10000);
+    var word = 'test';
+    return expect(collins(word)).to.eventually.equal(
+      'http://www.collinsdictionary.com/sounds/e/en_/en_us/en_us_test.mp3'
+    );
+  });
+
+  it('test case', function () {
+    this.timeout(10000);
+    var word = 'test case';
+    return expect(collins(word)).to.eventually.be.rejected;
+  });
+
+  it('Test-Drive', function () {
+    this.timeout(10000);
+    var word = 'Test-Drive';
+    return expect(collins(word)).to.eventually.equal(
+      'http://www.collinsdictionary.com/sounds/e/en_/en_us/en_us_test_drive.mp3'
+    );
+  });
+
+  it('askdjalksjdl', function () {
+    this.timeout(10000);
+    var word = 'askdjalksjdl';
+    return expect(collins(word)).to.eventually.be.rejected;
+  });
+
+  it('(null)', function () {
+    this.timeout(10000);
+    var word = null;
+    return expect(collins(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+  it('123 (number)', function () {
+    this.timeout(10000);
+    var word = 123;
+    return expect(collins(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+  it('123 (string)', function () {
+    this.timeout(10000);
+    var word = '123';
+    return expect(collins(word)).to.eventually.be.rejected;
+  });
+
+  it('(Empty String)', function () {
+    var word = '';
+    return expect(collins(word)).to.eventually.be.rejectedWith(TypeError);
+  });
+
+});
