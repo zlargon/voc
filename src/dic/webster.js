335f256948fb89579a26cad06ace8c236776c778
zlargon <zlargon@icloud.com>
2016-07-28 02:01:08 +0800

    refactor webster

--- src/dic/webster.js
+++ src/dic/webster.js
@@ -4,58 +4,89 @@ const fetch     = require('node-fetch');
 const cheerio   = require('cheerio');
 const resolve   = require('url').resolve;
 const normalize = require('../lib/normalize');
+const HOST      = 'http://www.merriam-webster.com';
+
+// 1. search url set
+const searchUrlSet = _async_(function * (word) {
+  const url = `${HOST}/autocomplete?term=${word}&ref=dictionary`;
+  const res = yield fetch(url, { timeout: 10 * 1000 });
+  if (res.status !== 200) {
+    throw new Error(`request to ${url} failed, status code = ${res.status} (${res.statusText})`);
+  }
+
+  /* Response Body:
+   *
+   * [{
+   *   "ref": "dictionary",
+   *   "term": "sherry",
+   *   "category": "Dictionary",
+   *   "label": "sherry",
+   *   "link": "/dictionary/sherry",
+   *   "show_cat": false,
+   *   "alsoIn": []
+   * }, {
+   *   "ref": "dictionary",
+   *   "term": "sherry",
+   *   "category": "English",
+   *   "label": "sherry",
+   *   "link": "http://www.spanishcentral.com/translate/sherry",
+   *   "show_cat": true,
+   *   "alsoIn": []
+   * }, {
+   *   "ref": "dictionary",
+   *   "term": "sherry",
+   *   "category": "Scrabble",
+   *   "label": "sherry",
+   *   "link": "http://scrabble.merriam.com/finder/sherry",
+   *   "show_cat": true,
+   *   "alsoIn": []
+   * }]
+   */
+
+  const set = {};
+  const list = yield res.json();
+  list.forEach(item => {
+    if (item.category === "Dictionary" && item.label.toLowerCase() === word) {
+      const link = resolve(HOST, item.link);
+      set[link] = true;
+    }
+  });
+  return set;
+});
+
+// 2. get audio set
+const getAudioSet = _async_(function * (wordUrl) {
+  const res = yield fetch(wordUrl, { timeout: 10 * 1000 });
+  if (res.status !== 200) {
+    throw new Error(`request to ${wordUrl} failed, status code = ${res.status} (${res.statusText})`);
+  }
+
+  const set = {};
+  const html = yield res.text();
+  const $ = cheerio.load(html);
+  $('.word-and-pronunciation .play-pron').each((index, element) => {
+    const ele = $(element);
+    const lang = ele.attr('data-lang').replace(/_/g, '/');
+    const dir  = ele.attr('data-dir');
+    const file = ele.attr('data-file');
+    const audio = `http://media.merriam-webster.com/audio/prons/${lang}/mp3/${dir}/${file}.mp3`;
+    set[audio] = true;
+  });
+
+  return set;
+});
 
 module.exports = _async_(function * (word) {
   word = normalize(word);
 
-  const HOST = 'http://www.merriam-webster.com';
-  const url = `${HOST}/autocomplete?term=${word}&ref=dictionary`;
-  const res = yield fetch(url, {
-    timeout: 10 * 1000
-  });
-  if (res.status !== 200) {
-    let err = new Error(`request to ${url} failed, status code = ${res.status} (${res.statusText})`);
-    err.code = 'ENOENT';
-    throw err;
-  }
-  const urlList = (yield res.json()).reduce((arr, item) => {
-    if (item.label.toLowerCase() === word) {
-      // link: "/dictionary/sherry"
-      // link: "http://www.spanishcentral.com/translate/sherry"
-      const link = resolve(HOST, item.link);
-      arr.push(link);
-    }
-    return arr;
-  }, []);
-
-  let map = {};
-  for (let i = 0; i < urlList.length; i++) {
-    const url = urlList[i];
-    const res = yield fetch(url, {
-      timeout: 10 * 1000
-    });
-    if (res.status !== 200) {
-      console.error(`request to ${url} failed, status code = ${res.status} (${res.statusText})`);
-      continue;
-    }
-
-    const $ = cheerio.load(yield res.text());
-    $('.word-and-pronunciation .play-pron').each((index, element) => {
-      const ele = $(element);
-      const lang = ele.attr('data-lang').replace(/_/g, '/');
-      const dir  = ele.attr('data-dir');
-      const file = ele.attr('data-file');
-      const audio = `http://media.merriam-webster.com/audio/prons/${lang}/mp3/${dir}/${file}.mp3`;
-
-      if (map.hasOwnProperty(audio) === false) {
-        map[audio] = true;
-      }
-    });
+  const set = {};
+  for (const url in yield searchUrlSet(word)) {
+    Object.assign(set, yield getAudioSet(url));
   }
 
-  const list = Object.keys(map);
+  const list = Object.keys(set);
   if (list.length === 0) {
-    let err = new Error(word + ' is not found from webster');
+    const err = new Error(`${word} is not found from webster`);
     err.code = 'ENOENT';
     throw err;
   }
@@ -63,7 +94,7 @@ module.exports = _async_(function * (word) {
   // show all the audio url
   if (list.length > 1) {
     list.forEach((audio, i) => {
-      console.log(`${i+1}. ${audio}`);
+      console.log(`${i + 1}. ${audio}`);
     });
   }
 
